f499158e9415e34c488bdb522ac4064bab48160e
nsjadon29 <nsjadon29@gmail.com>
2024-07-26 23:07:50 +0530

    added some sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Social from './components/Social'
+import About from './components/About'
 function App() {
   return (
     <>
      <Navbar />
      <Home />
+
+     <Social />
+     <About />
 
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImage from "../assets/heroImage.jpg";
+import heroImage from "../assets/heroImage.jpeg";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
@@ -10,25 +10,14 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="ext-4xl sm:text-7xl font-bold text-white">I'm a front-end developer</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a front-end developer</h2>
           <p className="text-gray-500 py-4 max-w-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint facere
-            id doloremque. Adipisci facere voluptates sed odit corporis omnis
-            ipsum esse eos provident illum? Saepe ipsa itaque ullam ut quibusdam
-            ratione nesciunt voluptate molestiae porro ex, totam obcaecati
-            necessitatibus iusto tempore mollitia quisquam minima, quaerat
-            facere numquam officia dicta ad odio nihil. Accusamus illum ducimus
-            numquam amet laboriosam, voluptatem facilis, molestias nostrum modi
-            nulla adipisci ipsum at nisi iure, recusandae sed. Architecto
-            magnam, fugiat consequatur consequuntur itaque neque at deserunt
-            quidem quibusdam sequi nostrum reprehenderit delectus quaerat ullam
-            odit facilis voluptate ex!
-          </p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nemo fugiat tempore ratione voluptatum odio?          </p>
           <div>
-            <button>
+            <button className="group text-white wi-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
-              <span>
-                <MdKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
           </div>
